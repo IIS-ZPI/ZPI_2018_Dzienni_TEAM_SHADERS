6279c4e18abb198a6525c6e7b4cfbe5cb2d70e4d
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-04-18 11:52:11 +0200

    Added Product Category class

---
+++ app/src/main/java/com/example/gucio/salestaxapp/ProducktCategory.java
@@ -0,0 +1,47 @@
+package com.example.gucio.salestaxapp;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ProducktCategory {
+
+    private States states;
+    private HashMap<String, BigDecimal> groceries;
+    private HashMap<String, BigDecimal> preparedFood;
+    private HashMap<String, BigDecimal> presciptionDrugs;
+    private HashMap<String, BigDecimal> nonPresciptionDrugs;
+    private HashMap<String, BigDecimal> clothing;
+    private HashMap<String, BigDecimal> intangibles;
+
+    public void setGroceries()
+    {
+
+    }
+
+    public void setPreparedFood()
+    {
+
+    }
+
+    public void setPresciptionDrugs()
+    {
+
+    }
+
+    public void setNonPresciptionDrugs()
+    {
+
+    }
+
+    public void setClothing()
+    {
+
+    }
+
+    public void setIntangibles()
+    {
+
+    }
+}
