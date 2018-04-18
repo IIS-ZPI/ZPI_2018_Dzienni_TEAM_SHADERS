0c6cd93590f4dcaac7499f1d4b0b03fc29b756d2
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-04-18 11:20:47 +0200

    TaxCalculation change

--- app/src/main/java/com/example/gucio/salestaxapp/TaxCalculation.java
+++ app/src/main/java/com/example/gucio/salestaxapp/TaxCalculation.java
@@ -3,19 +3,21 @@ package com.example.gucio.salestaxapp;
 import java.math.BigDecimal;
 
 
-public class TaxCalculation {
+class TaxCalculation {
 
     private BigDecimal baseCost = new BigDecimal(10);
     private BigDecimal generalCost = new BigDecimal(12.155);
-    private BigDecimal profitMargin;
+    private States states;
 
-    public BigDecimal marginCalculation(BigDecimal tax)
+    BigDecimal marginCalculation(String stateName)
     {
+        BigDecimal tax = states.statesList.get(stateName);
+
         tax = tax.divide(new BigDecimal(100),2,BigDecimal.ROUND_CEILING);
         System.out.println(tax);
         baseCost = baseCost.add(baseCost.multiply(tax));
         System.out.println(baseCost);
-        profitMargin = generalCost.subtract(baseCost);
+        BigDecimal profitMargin = generalCost.subtract(baseCost);
         profitMargin = profitMargin.divide(new BigDecimal(1),4,BigDecimal.ROUND_CEILING);
 
         return profitMargin.stripTrailingZeros();
