f508f9fd080cce0165388b6b882164490167a16e
co0lster <202254@edu.p.lodz.pl>
2018-05-09 11:55:22 +0200

    Spinner gets the list of states

--- app/src/main/java/com/example/gucio/salestaxapp/MainActivity.java
+++ app/src/main/java/com/example/gucio/salestaxapp/MainActivity.java
@@ -1,48 +1,73 @@
 package com.example.gucio.salestaxapp;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private TextView taxValue;
     private ExpandableListView state;
     private EditText income;
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
+    SpinnerAdapter spinnerAdapter;
+    Spinner expListView;
     List<String> listDataHeader;
     HashMap<String, List<String>> listDataChild;
+    States states = new States();
 
     private void setupApp()
     {
         taxValue = findViewById(R.id.TaxValue);
-        state = findViewById(R.id.State);
-        income = findViewById(R.id.Income);
+       // state = findViewById(R.id.State);
+       // income = findViewById(R.id.TaxValue);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupApp();
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.State);
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
-        // preparing list data
-        prepareListData();
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
+        // Spinner Drop down elements
+
+        List<String> listOfStates = states.getStatesList();
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listOfStates);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+        }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
     private void prepareListData()
@@ -55,14 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Adding child data
 
-        List<String> state = new ArrayList<String>();
-        state.add("Alabama");
-        state.add("Georgia");
-        state.add("Indiana");
-        state.add("Louisiana");
-        state.add("Nebraska");
+       List<String> listOfStates = states.getStatesList();
 
-        listDataChild.put(listDataHeader.get(0), state); // Header, Child data
+        listDataChild.put(listDataHeader.get(0), listOfStates); // Header, Child data
 
     }
 }
